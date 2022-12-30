d811cdb27ebf8b3ec36bb9e9ffec6e4561e63083
NTC <trongcong96@gmail.com>
2022-12-30 15:03:06 +0700

    init

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hello</div>
+</template>
+
+<script>
+const count = ref(0)
+export default {
+  name: "about",
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <b>
+      This is an auto-imported component.
+    </b>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
